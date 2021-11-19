190073937c4155e619d1ba6b13a87b8cf5748fbb
Ingo Franzki <ifranzki@linux.ibm.com>
2021-11-19 11:45:10 +0100

    Add skeleton for a provider and configure options for it
    Build the provider only if OpenSSL 3.0 or later is available.
    By default, the engine is always built, but this can be disabled
    during configuration.
    
    Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

---
+++ src/provider/p_ibmca.c
@@ -0,0 +1,38 @@
+/*
+ * Copyright [2021-2022] International Business Machines Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <stdio.h>
+
+#include <openssl/core.h>
+#include <openssl/core_dispatch.h>
+
+static const OSSL_DISPATCH prov_fns[] = {
+    { 0, NULL }
+};
+
+int OSSL_provider_init(const OSSL_CORE_HANDLE *handle,
+                       const OSSL_DISPATCH *in,
+                       const OSSL_DISPATCH **out,
+                       void **provctx)
+{
+    *out = prov_fns;
+    *provctx = NULL;
+
+    fprintf(stderr, "**** OSSL_provider_init called *****\n");
+
+    return 1;
+}
