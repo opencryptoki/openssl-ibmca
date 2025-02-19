opencryptoki/openssl-ibmca

commit 6bc53d814762b24045bfd5bb6003949a163fa58b

engine: Do not report errors if libica does not support EC key
In case EC key creation using libica fails (for whatever reason), and
the software fallback is used to perform the operation, do not report
the errors that have been put to the error stack by the failing attempt
to use libica.

Fixes: a462093d2478b287cb9a7a25131788eba16b7640

Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

src/engine/ibmca_ec.c
@@ -337,7 +337,9 @@ int ibmca_ecdh_compute_key(unsigned char **pout, size_t *poutlen,
     }
 
     /* Create ICA_EC_KEY object for private key */
+    ERR_set_mark();
     ica_privkey = ibmca_ec_make_and_cache_ica_key((EC_KEY*)ecdh, &privlen);
+    ERR_pop_to_mark();
     if (ica_privkey == NULL) {
         /* This curve is not supported by libica. */
     #ifdef OLDER_OPENSSL
@@ -502,7 +504,9 @@ ECDSA_SIG *ibmca_ecdsa_sign_sig(const unsigned char *dgst, int dgst_len,
     }
 
     /* Create ICA_EC_KEY object */
+    ERR_set_mark();
     icakey = ibmca_ec_make_and_cache_ica_key(eckey, &privlen);
+    ERR_pop_to_mark();
     if (icakey == NULL) {
         /* This curve is not supported by libica. */
  #ifdef OLDER_OPENSSL
@@ -633,7 +637,9 @@ int ibmca_ecdsa_verify_sig(const unsigned char *dgst, int dgst_len,
     }
 
     /* Create ICA_EC_KEY object */
+    ERR_set_mark();
     icakey = ibmca_ec_make_and_cache_ica_key(eckey, &privlen);
+    ERR_pop_to_mark();
     if (icakey == NULL) {
         /* This curve is not supported by libica. */
  #ifdef OLDER_OPENSSL

commit ee401a64ff35cf9e6d350533b2c195dfb495ca80

Fix compiler error for undefined ERR_pop_to_mark
    ibmca_ec.c:342:5: error: implicit declaration of function
           'ERR_pop_to_mark' [-Wimplicit-function-declaration]
      342 |     ERR_pop_to_mark();
          |     ^~~~~~~~~~~~~~~

Fixes: 6bc53d814762b24045bfd5bb6003949a163fa58b

Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

src/engine/ibmca_ec.c
@@ -17,6 +17,7 @@
 
 #include <stdlib.h>
 #include <pthread.h>
+#include <openssl/err.h>
 #include "ibmca.h"
 #include "e_ibmca_err.h"
 